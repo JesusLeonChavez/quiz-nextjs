822050b1010615fcfdfad0925b7126a4d74d4d70
Leonidas <jesus.leon1@unmsm.edu.pe>
2022-09-05 20:42:28 -0500

    feat: add data questions

---
+++ data/questions.ts
@@ -0,0 +1,40 @@
+const questions = [
+  {
+    title: "What is the capital of Venezuela?",
+    options: [
+      { textResponse: "Caracas", isCorrect: true },
+      { textResponse: "Valencia", isCorrect: false },
+      { textResponse: "Merida", isCorrect: false },
+      { textResponse: "Maracaibo", isCorrect: false },
+    ],
+  },
+  {
+    title: "What is the capital of Ecuador?",
+    options: [
+      { textResponse: "Ecuador", isCorrect: false },
+      { textResponse: "Quito", isCorrect: true },
+      { textResponse: "Trujillo", isCorrect: false },
+      { textResponse: "Guayaquil", isCorrect: false },
+    ],
+  },
+  {
+    title: "What is the capital of Chile?",
+    options: [
+      { textResponse: "Santiago de Chile", isCorrect: true },
+      { textResponse: "Tarapaca", isCorrect: false },
+      { textResponse: "Valparaiso", isCorrect: false },
+      { textResponse: "Iquique", isCorrect: false },
+    ],
+  },
+  {
+    title: "What is the capital of Paraguay?",
+    options: [
+      { textResponse: "Luque", isCorrect: false },
+      { textResponse: "San Lorenzo", isCorrect: false },
+      { textResponse: "Lima", isCorrect: false },
+      { textResponse: "Asuncion", isCorrect: true },
+    ],
+  },
+];
+
+export default questions;
